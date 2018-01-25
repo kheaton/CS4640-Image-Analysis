8426cc717b464b4dc9722dc0a5840750332e5eeb
Kyle Heaton <kheaton@users.noreply.github.com>
2018-01-25 06:26:38 -0700

    Notes from class

--- A1/CS4640_add_noise.m
+++ A1/CS4640_add_noise.m
@@ -20,4 +20,24 @@ CS4640_add_noise(im,p_min,p_max,noise_type,a,b,mu,sigma2)
 % Spring 2018
 %
 
+[M, N] = size(im);
+imn = zeros(M, N);
+
+% rand(50, 50) => generates a random image of that size,
+% shift it to the range you need.
+% multiple this matrix by ((b-a) + a) * rand(M, N)
+% generate thee new image, but it has to be restricted to p_min and p_max, just truncate it
+
+%index = max(1, min(m, fx));
+%value = max(p_min, min(p_max, theValue))
+
+% randn(u, sigma2) shift => sigma * randn + u
+
+for r = 1 : M
+    for c = 1 : N
+        % imNoise = imc + noise 
+        % rand samples from uniform dist from 0 to 1
+        % 
+    end
+end
 end
